3639d41c79efae13b8608325b430368e7d7c93c2
sunshine <tajohnsonn@gmail.com>
2020-11-15 13:41:39 -0800

    added carousel + carousel images

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 // components
 import MyNavBar from "./components/my-navbar/mynavbar.component";
+import MyCarousel from "./components/my-carousel/my-carousel.component";
+
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <MyNavBar />
+      <MyCarousel />
       My Portfolio
     </div>
   );
---
+++ myportfolio/src/components/my-carousel/my-carousel.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Slide1 from "../../assets/img/carousel/slide1.jpg";
+import Slide2 from "../../assets/img/carousel/slide2.jpg";
+import Slide3 from "../../assets/img/carousel/slide3.jpg";
+
+import "./my-carousel.styles.css";
+
+const MyCarousel = () => {
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={Slide2} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={Slide3} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={Slide1} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
+
+export default MyCarousel;
