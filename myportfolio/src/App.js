ad8a23654db6efd2733d1a963e8f2774f96084a6
sunshine <tajohnsonn@gmail.com>
2020-11-16 20:37:30 -0800

    adding parallex

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-parallax";
+import Container from "react-bootstrap/Container";
 
 // components
 import MyNavBar from "./components/my-navbar/mynavbar.component";
@@ -15,7 +18,22 @@ const App = () => {
       <MyCarousel />
       <TitleMessage />
       {/* about me */}
-      <About />
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.jpg")}
+          bgImageAlt=""
+          strength={-240}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
     </div>
   );
 };
