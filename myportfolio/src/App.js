30d51df4c4449c4889188dd3caeed69b1a7b62a1
sunshine <tajohnsonn@gmail.com>
2020-11-16 20:03:29 -0800

    completed about me section, import to app.js

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -5,7 +5,7 @@ import MyNavBar from "./components/my-navbar/mynavbar.component";
 import MyCarousel from "./components/my-carousel/my-carousel.component";
 import TitleMessage from "./components/title-message/title-message.component";
 import ScrollDown from "./components/scroll-down/scroll-down.component";
-
+import About from "./pages/about/about.components";
 import "./App.css";
 
 const App = () => {
@@ -14,8 +14,8 @@ const App = () => {
       <MyNavBar />
       <MyCarousel />
       <TitleMessage />
-      <TitleMessage />
-      <ScrollDown />
+      {/* about me */}
+      <About />
     </div>
   );
 };
---
+++ myportfolio/src/pages/about/about.components.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Profile from "../../assets/img/profile/t-aboutme.jpeg";
+
+// import "./about.style.class";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                  thumbnail
+                  fluid
+                />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hey there! I am <strong>&nbsp;Taylor J.</strong>
+                <br />I am a passionate and curious Front-End Web Developer in
+                Los Angeles, California.
+                <br />
+                I have a passion for tech and creating applications that are
+                innovative and user friendly
+                <br />
+                I bring professionalism, reliability, and integrity to the
+                table.
+                <br />
+                I love learning and accept challenges head on.
+                <br /> <br />
+                {/* social buttons start here */}
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's Chat!
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://drive.google.com/file/d/13_uKwP2a8z1fT9TegC_8c-SsT9E81bB6/view?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/tajohnsonn"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://www.linkedin.com/in/tajohnsonn/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
