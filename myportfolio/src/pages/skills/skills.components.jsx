bab0d93c2fc61b10b7746c79e40efb3500a9ab27
sunshine <tajohnsonn@gmail.com>
2020-11-17 05:33:33 -0800

    starting skills section

---
+++ myportfolio/src/pages/skills/skills.components.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+
+const Skills = () => {
+    return (
+        <div className="pt-3 pb-3" id="skills">
+            <h1 className="text-center font-details-b pb-4">TECH SKILLS</h1>
+        </div>
+};
+
+export default Skills;
